0090727e9e8f8731893ecd83931fa75a9b657105
tozggg <tozggg@gmail.com>
2022-06-14 16:04:59 +0900

    modmod

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ ! -e /var/www/wordpress ]; then
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
 	mkdir -p /var/www/wordpress
-	wp core download --allow-root --version=5.9.1 --path='/var/www/wordpress'
+	wp core download --allow-root --version=5.8.1 --path='/var/www/wordpress'
 	chown -R www-data:www-data /var/www/wordpress
 
 	wp config create	--allow-root --dbname=$DB_NAME --dbuser=$DB_USER\
 						--dbpass=$DB_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
 	
-	wp core install     --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER1_NAME\
-                        --admin_password=$WP_USER1_PASSWORD --admin_email=$WP_USER1_EMAIL --path='/var/www/wordpress'
+	wp core install     --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --path='/var/www/wordpress'\
+						--admin_user=$WP_USER1_NAME --admin_password=$WP_USER1_PASSWORD --admin_email=$WP_USER1_EMAIL 
     
-	wp user create      --allow-root $WP_USER2_NAME $WP_USER2_EMAIL --user_pass=$WP_USER2_PASSWORD --role=author\
-                        --path='/var/www/wordpress'
+	wp user create      --allow-root $WP_USER2_NAME $WP_USER2_EMAIL --user_pass=$WP_USER2_PASSWORD\
+						--role=author --path='/var/www/wordpress'
 fi
 
 mkdir -p /run/php
