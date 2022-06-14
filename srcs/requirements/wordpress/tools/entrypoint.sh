1ee0e609d64fe34ffc1fc689760f0996e92765a6
tozggg <tozggg@gmail.com>
2022-06-14 18:44:57 +0900

    momodmodmo

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -7,10 +7,10 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 
 	wp config create	--allow-root --dbname=$DB_NAME --dbuser=$DB_USER\
 						--dbpass=$DB_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
-	
+
 	wp core install     --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --path='/var/www/wordpress'\
 						--admin_user=$WP_USER1_NAME --admin_password=$WP_USER1_PASSWORD --admin_email=$WP_USER1_EMAIL 
-    
+
 	wp user create      --allow-root $WP_USER2_NAME $WP_USER2_EMAIL --user_pass=$WP_USER2_PASSWORD\
 						--role=author --path='/var/www/wordpress'
 fi
